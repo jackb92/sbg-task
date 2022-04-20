da053cd7696bf960ba845c9cb57cea1a9ea70911
Jack <jackbentley1992@gmail.com>
2022-04-20 15:13:18 +0100

    Added initial api tests

---
+++ cypress/integration/tests.js
@@ -0,0 +1,27 @@
+describe('Hitting the /football/live endpoint should return all events as football events', () => {
+    it('Given the base url is up', () => {
+
+    }),
+
+    it('When I hit the /football/live endpoint', () => {
+
+    }),
+
+    it('Then all events returned are football events', () => {
+
+    })
+})
+
+describe('Hitting the /football/live endpoint should return a home and away competitor for each event', () => {
+    it('Given the base url is up', () => {
+
+    }),
+
+    it('When I hit the /football/live endpoint', () => {
+
+    }),
+
+    it('Then each event should have a home & away competitor', () => {
+
+    })
+})
