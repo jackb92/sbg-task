70348a77e80569a3398d737e5dcafdf54c0e39fa
Jack <jackbentley1992@gmail.com>
2022-04-20 22:39:33 +0100

    refactored code to write response body to fixtures & added commands for duplicated code

--- cypress/integration/task_1_tests/task_1_tests.js
+++ cypress/integration/task_1_tests/task_1_tests.js
@@ -1,31 +1,32 @@
-import { Given, When, Then, And } from 'cypress-cucumber-preprocessor/steps'
+import { Given, When, Then, And, Before } from 'cypress-cucumber-preprocessor/steps'
 import {baseUrl, footballLiveEndPoint} from '../../fixtures/urls.js'
 
-let footballLiveEndPointBody
+Before(() => {
+    cy.WriteResponseBodyFileToFixtures(`${baseUrl}${footballLiveEndPoint}`, '../fixtures/footballLiveEndPointBody.json')
+});
 
 Given('The base url is up', () => {
-    cy.request(baseUrl).then((response) => {
-        expect(response.status).to.eq(200)
-    })
+    cy.assertStatusCode(baseUrl, 200)
 })
 
 When('I hit the {string} endpoint', () => {
-    cy.request(`${baseUrl}${footballLiveEndPoint}`).then((response) => {
-        expect(response.status).to.eq(200)
-        footballLiveEndPointBody = response.body
-    })
+    cy.assertStatusCode(`${baseUrl}${footballLiveEndPoint}`, 200)
 })
 
 Then('All events returned are football events', () => {
-    footballLiveEndPointBody.events.map(event => {
-        expect(event.className).to.eq('Football')
-        expect(event.typeName).to.eq('Football Live')
+    cy.readFile('../fixtures/footballLiveEndPointBody.json').then((footballLiveEndPointBody) => {
+        footballLiveEndPointBody.events.map(event => {
+            expect(event.className).to.eq('Football')
+            expect(event.typeName).to.eq('Football Live')
+        })
     })
 })
 
 And('Each event should have a home & away competitor', () => {
-    footballLiveEndPointBody.events.map(event => {
-        expect(event.competitors[0].position).to.eq('home')
-        expect(event.competitors[1].position).to.eq('away')
+    cy.readFile('../fixtures/footballLiveEndPointBody.json').then((footballLiveEndPointBody) => {
+        footballLiveEndPointBody.events.map(event => {
+            expect(event.competitors[0].position).to.eq('home')
+            expect(event.competitors[1].position).to.eq('away')
+        })
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,21 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('assertStatusCode', (url, statusCode) => {
+    cy.request(url).then((response) => {
+        expect(response.status).to.eq(statusCode)
+    })
+})
+
+Cypress.Commands.add('WriteResponseBodyFileToFixtures', (url, pathToFixtures) => {
+    cy.request(url).then((response) => {
+        const body = response.body
+        cy.writeFile(pathToFixtures, body)
+    })
+})
+
+// Cypress.Commands.add('AssertCorrectEventResponseReturned', (pathToFile,) => {
+//     cy.readFile('../fixtures/footballLiveEndPointBody.json').then((body) => {
+//         body.events.map(event => {
+//             expect(event.className).to.eq('Football')
+//             expect(event.typeName).to.eq('Football Live')
+//         })
+//     })
+// })
