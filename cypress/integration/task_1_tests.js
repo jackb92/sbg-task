8ab5352ace43f66590d9e9b5dd6fae912b67e84f
Jack <jackbentley1992@gmail.com>
2022-04-20 20:45:11 +0100

    refactored code to use variable urls

--- cypress/integration/task_1_tests.js
+++ cypress/integration/task_1_tests.js
@@ -1,18 +1,20 @@
-describe('Hitting the /football/live endpoint should return all events as football events', () => {
+import {baseUrl, footballLiveEndPoint} from '../fixtures/urls.js'
+
+describe(`Hitting the ${footballLiveEndPoint} endpoint should return all events as football events`, () => {
     it('Given the base url is up', () => {
-        cy.request('http://localhost:8888/').then((response) => {
+        cy.request(baseUrl).then((response) => {
             expect(response.status).to.eq(200);
         })
     }),
 
-    it('When I hit the /football/live endpoint', () => {
-        cy.request('http://localhost:8888/football/live').then((response) => {
+    it(`When I hit the ${footballLiveEndPoint} endpoint`, () => {
+        cy.request(`${baseUrl}${footballLiveEndPoint}`).then((response) => {
             expect(response.status).to.eq(200);
         })
     }),
 
     it('Then all events returned are football events', () => {
-        cy.request('http://localhost:8888/football/live').then((response) => {
+        cy.request(`${baseUrl}${footballLiveEndPoint}`).then((response) => {
             const body = response.body
             body.events.map(event => {
                 expect(event.className).to.eq('Football')
@@ -22,21 +24,21 @@ describe('Hitting the /football/live endpoint should return all events as footba
     })
 })
 
-describe.only('Hitting the /football/live endpoint should return a home and away competitor for each event', () => {
+describe.only(`Hitting the ${footballLiveEndPoint} endpoint should return a home and away competitor for each event`, () => {
     it('Given the base url is up', () => {
-        cy.request('http://localhost:8888/').then((response) => {
+        cy.request(baseUrl).then((response) => {
             expect(response.status).to.eq(200);
         })
     }),
 
-    it('When I hit the /football/live endpoint', () => {
-        cy.request('http://localhost:8888/').then((response) => {
+    it(`When I hit the ${footballLiveEndPoint} endpoint`, () => {
+        cy.request(`${baseUrl}${footballLiveEndPoint}`).then((response) => {
             expect(response.status).to.eq(200);
         })
     }),
 
     it('Then each event should have a home & away competitor', () => {
-        cy.request('http://localhost:8888/football/live').then((response) => {
+        cy.request(`${baseUrl}${footballLiveEndPoint}`).then((response) => {
             const body = response.body
             body.events.map(event => {
                 expect(event.competitors[0].position).to.eq('home')
