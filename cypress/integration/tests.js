029fb76238d7dc51be0cf801c92cfb3af00e803f
Jack <jackbentley1992@gmail.com>
2022-04-20 20:36:11 +0100

    Added code to pass test that asserts home & away is returned in body

--- cypress/integration/tests.js
+++ cypress/integration/tests.js
@@ -2,13 +2,13 @@ describe('Hitting the /football/live endpoint should return all events as footba
     it('Given the base url is up', () => {
         cy.request('http://localhost:8888/').then((response) => {
             expect(response.status).to.eq(200);
-        });
+        })
     }),
 
     it('When I hit the /football/live endpoint', () => {
         cy.request('http://localhost:8888/football/live').then((response) => {
             expect(response.status).to.eq(200);
-        });
+        })
     }),
 
     it('Then all events returned are football events', () => {
@@ -16,21 +16,32 @@ describe('Hitting the /football/live endpoint should return all events as footba
             const body = response.body
             body.events.map(event => {
                 expect(event.className).to.eq('Football')
+                expect(event.typeName).to.eq('Football Live')
             })
-        });
+        })
     })
 })
 
-describe('Hitting the /football/live endpoint should return a home and away competitor for each event', () => {
+describe.only('Hitting the /football/live endpoint should return a home and away competitor for each event', () => {
     it('Given the base url is up', () => {
-
+        cy.request('http://localhost:8888/').then((response) => {
+            expect(response.status).to.eq(200);
+        })
     }),
 
     it('When I hit the /football/live endpoint', () => {
-
+        cy.request('http://localhost:8888/').then((response) => {
+            expect(response.status).to.eq(200);
+        })
     }),
 
     it('Then each event should have a home & away competitor', () => {
-
+        cy.request('http://localhost:8888/football/live').then((response) => {
+            const body = response.body
+            body.events.map(event => {
+                expect(event.competitors[0].position).to.eq('home')
+                expect(event.competitors[1].position).to.eq('away')
+            })
+        })
     })
 })
