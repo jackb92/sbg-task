b3ceb15717c7d981b5aaf9e625a4b0b39b9ad37e
Jack <jackbentley1992@gmail.com>
2022-04-20 15:55:06 +0100

    Written passing test for first scenario

--- cypress/integration/tests.js
+++ cypress/integration/tests.js
@@ -1,14 +1,23 @@
 describe('Hitting the /football/live endpoint should return all events as football events', () => {
     it('Given the base url is up', () => {
-
+        cy.request('http://localhost:8888/').then((response) => {
+            expect(response.status).to.eq(200);
+        });
     }),
 
     it('When I hit the /football/live endpoint', () => {
-
+        cy.request('http://localhost:8888/football/live').then((response) => {
+            expect(response.status).to.eq(200);
+        });
     }),
 
     it('Then all events returned are football events', () => {
-
+        cy.request('http://localhost:8888/football/live').then((response) => {
+            const body = response.body
+            body.events.map(event => {
+                expect(event.className).to.eq('Football')
+            })
+        });
     })
 })
 
