9011842dae34b92ef9d679cf02dbeee494b239cd
Jack <jackbentley1992@gmail.com>
2022-04-20 20:51:23 +0100

    refactored to remove redundent code

--- cypress/integration/task_1_tests.js
+++ cypress/integration/task_1_tests.js
@@ -1,4 +1,5 @@
 import {baseUrl, footballLiveEndPoint} from '../fixtures/urls.js'
+let footballLiveEndPointBody
 
 describe(`Hitting the ${footballLiveEndPoint} endpoint should return all events as football events`, () => {
     it('Given the base url is up', () => {
@@ -10,40 +11,45 @@ describe(`Hitting the ${footballLiveEndPoint} endpoint should return all events 
     it(`When I hit the ${footballLiveEndPoint} endpoint`, () => {
         cy.request(`${baseUrl}${footballLiveEndPoint}`).then((response) => {
             expect(response.status).to.eq(200);
+            footballLiveEndPointBody = response.body
         })
     }),
 
     it('Then all events returned are football events', () => {
-        cy.request(`${baseUrl}${footballLiveEndPoint}`).then((response) => {
-            const body = response.body
-            body.events.map(event => {
-                expect(event.className).to.eq('Football')
-                expect(event.typeName).to.eq('Football Live')
-            })
+        footballLiveEndPointBody.events.map(event => {
+            expect(event.className).to.eq('Football')
+            expect(event.typeName).to.eq('Football Live')
+        })
+    })
+
+    it('And each event should have a home & away competitor', () => {
+        footballLiveEndPointBody.events.map(event => {
+            expect(event.competitors[0].position).to.eq('home')
+            expect(event.competitors[1].position).to.eq('away')
         })
     })
 })
 
-describe.only(`Hitting the ${footballLiveEndPoint} endpoint should return a home and away competitor for each event`, () => {
-    it('Given the base url is up', () => {
-        cy.request(baseUrl).then((response) => {
-            expect(response.status).to.eq(200);
-        })
-    }),
+// describe.only(`Hitting the ${footballLiveEndPoint} endpoint should return a home and away competitor for each event`, () => {
+//     it('Given the base url is up', () => {
+//         cy.request(baseUrl).then((response) => {
+//             expect(response.status).to.eq(200);
+//         })
+//     }),
 
-    it(`When I hit the ${footballLiveEndPoint} endpoint`, () => {
-        cy.request(`${baseUrl}${footballLiveEndPoint}`).then((response) => {
-            expect(response.status).to.eq(200);
-        })
-    }),
+//     it(`When I hit the ${footballLiveEndPoint} endpoint`, () => {
+//         cy.request(`${baseUrl}${footballLiveEndPoint}`).then((response) => {
+//             expect(response.status).to.eq(200);
+//         })
+//     }),
 
-    it('Then each event should have a home & away competitor', () => {
-        cy.request(`${baseUrl}${footballLiveEndPoint}`).then((response) => {
-            const body = response.body
-            body.events.map(event => {
-                expect(event.competitors[0].position).to.eq('home')
-                expect(event.competitors[1].position).to.eq('away')
-            })
-        })
-    })
-})
+//     it('Then each event should have a home & away competitor', () => {
+//         cy.request(`${baseUrl}${footballLiveEndPoint}`).then((response) => {
+//             const body = response.body
+//             body.events.map(event => {
+//                 expect(event.competitors[0].position).to.eq('home')
+//                 expect(event.competitors[1].position).to.eq('away')
+//             })
+//         })
+//     })
+//})
