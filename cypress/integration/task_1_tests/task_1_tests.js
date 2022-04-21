a13c393b6fc63b181d8189e24a0803fa6c4f114b
Jack <jackbentley1992@gmail.com>
2022-04-21 12:14:52 +0100

    removed redundent code

--- cypress/integration/task_1_tests/task_1_tests.js
+++ cypress/integration/task_1_tests/task_1_tests.js
@@ -14,7 +14,6 @@ When('I hit the {string} endpoint', () => {
     cy.assertStatusCode(`${baseUrl}${footballLiveEndPoint}`, 200)
 })
 
-//ToDo - duplicated code, need to be more dynamic but some complexity around what may be an unspecified amount of assertions
 Then('All events returned are football events', () => {
     cy.readFile('../fixtures/footballLiveEndPointBody.json').then((footballLiveEndPointBody) => {
         assertEvents(footballLiveEndPointBody, 2, 'className', 'Football','typeName', 'Football Live')
@@ -24,9 +23,5 @@ Then('All events returned are football events', () => {
 And('Each event should have a home & away competitor', () => {  
     cy.readFile('../fixtures/footballLiveEndPointBody.json').then((footballLiveEndPointBody) => {
         assertEvents(footballLiveEndPointBody, 2, 'competitors', 'position','home','competitors', 'position','away')
-        //footballLiveEndPointBody.events.map(event => {
-            // expect(event.competitors[0].position).to.eq('home')
-            // expect(event.competitors[1].position).to.eq('away')
-        //})
     })
 })
